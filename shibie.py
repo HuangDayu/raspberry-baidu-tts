06b39ad29ceff904fc8c9bb301db0547c8f5cf88
huangdayu <1161946342@qq.com>
2018-03-03 14:51:44 +0800

    黄大宇

--- shibie.py
+++ shibie.py
@@ -1,29 +1,44 @@
-#!/usr/bin/env python#coding:utf-8# -*- coding: utf-8 -*
+#!/usr/bin/env python
+#coding:utf-8
+# -*- coding: utf-8 -*
+
 import os
-from api import AipSpeech
+from api import AipSpeech
+
 #App ID: 10351927
 #API Key: VWgKFTtRGdeviVbZ0AP80SfZ
 #Secret Key: 1945bd34ec52542c48a4864b76bda2ca
-#语音识别百度文档地址：http://yuyin.baidu.com/docs/asr/190
-# arecord -Dhw:1,0 -c 2 -r 16000 -f S16_LE test.wav
+#语音识别百度文档地址：http://yuyin.baidu.com/docs/asr/190
+
+# arecord -Dhw:1,0 -c 2 -r 16000 -f S16_LE test.wav
+
 APP_ID = '10351927'
 API_KEY = 'VWgKFTtRGdeviVbZ0AP80SfZ'
 SECRET_KEY = '1945bd34ec52542c48a4864b76bda2ca'
-aipSpeech = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+aipSpeech = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+
 # 读取文件
 def get_file_content(filePath):
     with open(filePath, 'rb') as fp:
-        return fp.read()
-
+        return fp.read()
+
+
+
 # 从URL获取文件识别
 # aipSpeech.asr('', 'pcm', 16000, {
 #     'url': 'http://121.40.195.233/res/16k_test.pcm',
 #     'callback': 'http://xxx.com/receive',
-# })
-
+# })
+
+
+
 if __name__ == "__main__":
     # 识别本地文件
     str1 = aipSpeech.asr(get_file_content('./shibie.wav'), 'wav',16000,{
         'lan': 'zh',
-    })	str2=str1['result']	str3=str2[0]
-    print(str3)#.encode('utf-8')#
+    })
+    str2=str1['result']
+    str3=str2[0]
+    print(str3)
+#.encode('utf-8')
+#
